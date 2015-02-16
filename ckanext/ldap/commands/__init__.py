5dd0b21433af02af73fed9eba14623ddc55cbd0c
benscott <ben@benscott.co.uk>
2015-02-16 17:37:49 +0000

    Adding set up org paster command

---
+++ ckanext/ldap/commands/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Created by 'bens3' on 2013-06-21.
+Copyright (c) 2013 'bens3'. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ ckanext/ldap/commands/ldap.py
@@ -0,0 +1,59 @@
+
+import logging
+import pylons
+from ckan.lib.cli import CkanCommand
+from ckanext.datastore.db import _get_engine
+from ckan.plugins import toolkit
+from ckan import logic
+
+log = logging.getLogger()
+
+class LDAPCommand(CkanCommand):
+    """
+
+    Paster function to set up the default organisation
+
+    Paster function can be included to provision scripts - otherwise, get an error after provisioning new CKAN instance
+
+    Commands:
+
+        paster ldap setup-org -c /etc/ckan/default/development.ini
+
+    """
+    summary = __doc__.split('\n')[0]
+    usage = __doc__
+
+
+    def command(self):
+
+        if not self.args or self.args[0] in ['--help', '-h', 'help']:
+            print self.__doc__
+            return
+
+        self._load_config()
+
+        # Set up context
+        user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
+        self.context = {'user': user['name']}
+
+        # Set up datastore DB engine
+        self.engine = _get_engine({
+            'connection_url': pylons.config['ckan.datastore.write_url']
+        })
+
+        cmd = self.args[0]
+
+        if cmd == 'setup-org':
+            self.setup_org()
+        else:
+            print 'Command %s not recognized' % cmd
+
+    def setup_org(self):
+
+        # Get the organisation all users will be added to
+        organization_id = pylons.config['ldap.organization.id']
+
+        try:
+            toolkit.get_action('organization_show')(self.context, {'id': organization_id})
+        except logic.NotFound:
+            toolkit.get_action('organization_create')(self.context, {'id': organization_id, 'name': organization_id})
