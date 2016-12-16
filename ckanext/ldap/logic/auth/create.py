9d71c534ac0d2ee974fe06faf13f083e5d7bea0c
Georg Seyerl <georg.seyerl@gmail.com>
2016-12-16 13:40:37 +0100

    Fix encoding for ldap request

--- ckanext/ldap/logic/auth/create.py
+++ ckanext/ldap/logic/auth/create.py
@@ -7,7 +7,7 @@ from ckanext.ldap.controllers.user import _find_ldap_user
 @ckan.logic.auth_allow_anonymous_access
 def user_create(context, data_dict=None):
     if data_dict and 'name' in data_dict:
-        ldap_user_dict = _find_ldap_user(data_dict['name'])
+        ldap_user_dict = _find_ldap_user(data_dict['name'].encode('utf-8'))
         if ldap_user_dict:
             return {'success': False, 'msg': _('An LDAP user by that name already exists')}
 
