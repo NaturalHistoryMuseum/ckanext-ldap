59e2d1ce9c770359816dd8c6c06dade147d32e7d
anja.stemme@zamg.ac.at <anja.stemme@zamg.ac.at>
2017-06-22 17:45:25 +0200

    removed test-organization

--- ckanext/ldap/logic/action.py
+++ ckanext/ldap/logic/action.py
@@ -147,8 +147,8 @@ def add_ckan_user(context,data_dict):
         return "Hooray! User " + user_name + " successfully created in CKAN (no Organization was applicable)"
 
 def _check_mail_org(user_email):
-    ccca_orgs= [u'aau', u'ages', u'ait', u'alps', u'bayerische-akademie-der-wissenschaften', u'bfw-bundesforschungszentrum-fur-wald', u'boku', u'ccca', u'essl', u'gba', u'iiasa', u'iio', u'jr', u'oaw', u'ogm', u'tu-graz', u'tu-wien', u'uba', u'uibk', u'uma', u'uni-salzburg', u'uni-wien', u'vetmeduni', u'wegener-center', u'wifo', u'wp', u'wu', u'zamg', u'zsi',u'usertest-organization']
-    ccca_mails = [u'aau.at',u'ages.at',u'ait.ac.at',u'alps-gmbh.com',u'badw.de',u'bfw.gv.at',u'boku.ac.at', u'ccca.ac.at', u'essl.org',u'geologie.ac.at',u'iiasa.ac.at',u'indoek.at',u'joanneum.at', u'oeaw.ac.at', u'meteorologie.at', u'tugraz.at', u'tuwien.ac.at', u'umweltbundesamt.at',u'uibk.ac.at',u'uma.or.at',u'sbg.ac.at', u'univie.ac.at',u'vetmeduni.ac.at', u'uni-graz.at', u'wifo.ac.at', u'weatherpark.com', u'wu.ac.at',u'zamg.ac.at', u'zsi.at',u'none.at']
+    ccca_orgs= [u'aau', u'ages', u'ait', u'alps', u'bayerische-akademie-der-wissenschaften', u'bfw-bundesforschungszentrum-fur-wald', u'boku', u'ccca', u'essl', u'gba', u'iiasa', u'iio', u'jr', u'oaw', u'ogm', u'tu-graz', u'tu-wien', u'uba', u'uibk', u'uma', u'uni-salzburg', u'uni-wien', u'vetmeduni', u'wegener-center', u'wifo', u'wp', u'wu', u'zamg', u'zsi']
+    ccca_mails = [u'aau.at',u'ages.at',u'ait.ac.at',u'alps-gmbh.com',u'badw.de',u'bfw.gv.at',u'boku.ac.at', u'ccca.ac.at', u'essl.org',u'geologie.ac.at',u'iiasa.ac.at',u'indoek.at',u'joanneum.at', u'oeaw.ac.at', u'meteorologie.at', u'tugraz.at', u'tuwien.ac.at', u'umweltbundesamt.at',u'uibk.ac.at',u'uma.or.at',u'sbg.ac.at', u'univie.ac.at',u'vetmeduni.ac.at', u'uni-graz.at', u'wifo.ac.at', u'weatherpark.com', u'wu.ac.at',u'zamg.ac.at', u'zsi.at']
 
     #print len (ccca_orgs)
     #print len(ccca_mails)
@@ -168,12 +168,15 @@ def _check_mail_org(user_email):
         # check if subdomain
         if config.get('ckanext.ldap.mail_prefix'):
             prefixes = config['ckanext.ldap.mail_prefix']
-            #print prefixes
+            print prefixes
             dot_i = mail_to_check.find('.')
             if dot_i > 0:
                 subdo = mail_to_check[0:dot_i]
+                if subdo not in prefixes:
+                    return None
+
                 mail_to_check = mail_to_check[dot_i+1:]
-                #print subdo
+
                 #print mail_to_check
                 if mail_to_check in ccca_mails:
                     #print "success"
