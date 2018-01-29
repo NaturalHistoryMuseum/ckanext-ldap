6aa573169fc17354cb6539b3b6107b4d2d799fc5
Josh Humphries <j.humphries@nhm.ac.uk>
2018-01-29 12:30:09 +0000

    Add debug_level and trace_level options to plugin config schema

--- ckanext/ldap/plugin.py
+++ ckanext/ldap/plugin.py
@@ -74,7 +74,9 @@ class LdapPlugin(p.SingletonPlugin):
             'ckanext.ldap.organization.role': {'default': 'member', 'validate': _allowed_roles},
             'ckanext.ldap.ckan_fallback': {'default': False, 'parse': p.toolkit.asbool},
             'ckanext.ldap.prevent_edits': {'default': False, 'parse': p.toolkit.asbool},
-            'ckanext.ldap.migrate': {'default': False, 'parse': p.toolkit.asbool}
+            'ckanext.ldap.migrate': {'default': False, 'parse': p.toolkit.asbool},
+            'ckanext.ldap.debug_level': {'default': 0, 'parse': p.toolkit.asint},
+            'ckanext.ldap.trace_level': {'default': 0, 'parse': p.toolkit.asint},
         }
         errors = []
         for i in schema:
