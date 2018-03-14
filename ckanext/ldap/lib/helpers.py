1a213ae4ebfb410f1480b324f5fa13cb4a59ef25
Alice Butcher <alycejenni@gmail.com>
2018-03-14 17:52:46 +0000

    manual corrections for ldap.lib

--- ckanext/ldap/lib/helpers.py
+++ ckanext/ldap/lib/helpers.py
@@ -1,19 +1,18 @@
-
-#!/usr/bin/env python
+# !/usr/bin/env python
 # encoding: utf-8
 #
 # This file is part of ckanext-ldap
 # Created by the Natural History Museum in London, UK
 
-import pylons
+from ckan.common import session
 
 
 def is_ldap_user():
-    '''Help function for determining if current user is LDAP user
+    '''Helper function for determining if current user is LDAP user
 
 
     :returns: boolean
 
     '''
 
-    return u'ckanext-ldap-user' in pylons.session
+    return u'ckanext-ldap-user' in session
