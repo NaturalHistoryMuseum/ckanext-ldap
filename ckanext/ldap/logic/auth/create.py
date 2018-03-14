f251920e34335ca867b6f105249713a390a8f815
Alice Butcher <alycejenni@gmail.com>
2018-03-14 18:03:25 +0000

    manual corrections for ldap.logic

--- ckanext/ldap/logic/auth/create.py
+++ ckanext/ldap/logic/auth/create.py
@@ -4,23 +4,27 @@
 # This file is part of ckanext-ldap
 # Created by the Natural History Museum in London, UK
 
-import ckan.logic, ckan.logic.auth
-from ckan.common import _
-from ckan.logic.auth.create import user_create as ckan_user_create
 from ckanext.ldap.controllers.user import _find_ldap_user
 
+from ckan.plugins import toolkit
 
-@ckan.logic.auth_allow_anonymous_access
-def user_create(context, data_dict=None):
+
+@toolkit.chained_auth_function
+@toolkit.auth_allow_anonymous_access
+def user_create(next_auth, context, data_dict=None):
     '''
 
-    :param context: 
+    :param next_auth: the next auth function in the chain
+    :param context:
     :param data_dict:  (Default value = None)
 
     '''
     if data_dict and u'name' in data_dict:
         ldap_user_dict = _find_ldap_user(data_dict[u'name'])
         if ldap_user_dict:
-            return {u'success': False, u'msg': _(u'An LDAP user by that name already exists')}
+            return {
+                u'success': False,
+                u'msg': toolkit._(u'An LDAP user by that name already exists')
+                }
 
-    return ckan_user_create(context, data_dict)
+    return next_auth(context, data_dict)
--- ckanext/ldap/logic/auth/update.py
+++ ckanext/ldap/logic/auth/update.py
@@ -4,34 +4,45 @@
 # This file is part of ckanext-ldap
 # Created by the Natural History Museum in London, UK
 
-import ckan.logic, ckan.logic.auth
-from ckan.common import _
-from ckan.logic.auth.update import user_update as ckan_user_update
-from ckanext.ldap.plugin import config
-from ckanext.ldap.model.ldap_user import LdapUser
 from ckanext.ldap.controllers.user import _find_ldap_user
+from ckanext.ldap.model.ldap_user import LdapUser
+from ckanext.ldap.plugin import config
 
-@ckan.logic.auth_allow_anonymous_access
-def user_update(context, data_dict):
+from ckan.logic import auth
+from ckan.plugins import toolkit
+
+
+@toolkit.chained_auth_function
+@toolkit.auth_allow_anonymous_access
+def user_update(next_auth, context, data_dict):
     '''Ensure LDAP users cannot be edited, and name clash with ldap users
 
+    :param next_auth: the next auth function in the chain
     :param context: 
     :param data_dict: 
 
     '''
     user_obj = None
     try:
-        user_obj = ckan.logic.auth.get_user_object(context, data_dict)
-    except ckan.logic.NotFound:
+        user_obj = auth.get_user_object(context, data_dict)
+    except toolkit.ObjectNotFound:
         pass
     # Prevent edition of LDAP users (if so configured)
-    if config[u'ckanext.ldap.prevent_edits'] and user_obj and LdapUser.by_user_id(user_obj.id):
-        return {u'success': False, u'msg': _(u'Cannot edit LDAP users')}
+    if config[u'ckanext.ldap.prevent_edits'] and user_obj and LdapUser.by_user_id(
+            user_obj.id):
+        return {
+            u'success': False,
+            u'msg': toolkit._(u'Cannot edit LDAP users')
+            }
     # Prevent name clashes!
     if u'name' in data_dict and user_obj and user_obj.name != data_dict[u'name']:
         ldap_user_dict = _find_ldap_user(data_dict[u'name'])
         if ldap_user_dict:
-            if len(user_obj.ldap_user) == 0 or user_obj.ldap_user[0].ldap_id != ldap_user_dict[u'ldap_id']:
-                return {u'success': False, u'msg': _(u'An LDAP user by that name already exists')}
+            if len(user_obj.ldap_user) == 0 or user_obj.ldap_user[0].ldap_id != \
+                    ldap_user_dict[u'ldap_id']:
+                return {
+                    u'success': False,
+                    u'msg': toolkit._(u'An LDAP user by that name already exists')
+                    }
 
-    return ckan_user_update(context, data_dict)
+    return next_auth(context, data_dict)
