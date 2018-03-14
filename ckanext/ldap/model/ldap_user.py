a20d67a4b9275325ab7023dd1635f5e23f1039a0
Alice Butcher <alycejenni@gmail.com>
2018-03-14 18:11:39 +0000

    manual corrections for ldap.model

--- ckanext/ldap/model/ldap_user.py
+++ ckanext/ldap/model/ldap_user.py
@@ -6,29 +6,32 @@
 
 import datetime
 
-import ckan.model.meta as meta
-import ckan.model.user as user
-import ckan.model.domain_object as domain_object
-import ckan.model.types as _types
+from sqlalchemy import Column, ForeignKey, Table, orm, types
 
-from sqlalchemy import orm, types, Column, Table, ForeignKey
+from ckan import model
 
 __all__ = [u'LdapUser']
 
-ldap_user_table = Table(u'ldap_user', meta.metadata,
-                     Column(u'id', types.UnicodeText, primary_key=True, default=_types.make_uuid),
-                     Column(u'user_id', types.UnicodeText, ForeignKey(u'user.id'), unique=True, nullable=False),
-                     Column(u'ldap_id', types.UnicodeText, index=True, unique=True, nullable=False),
-                     Column(u'created', types.DateTime, default=datetime.datetime.now)
-                     )
+ldap_user_table = Table(u'ldap_user', model.meta.metadata,
+                        Column(u'id', types.UnicodeText, primary_key=True,
+                               default=model.types.make_uuid),
+                        Column(u'user_id', types.UnicodeText, ForeignKey(u'user.id'),
+                               unique=True, nullable=False),
+                        Column(u'ldap_id', types.UnicodeText, index=True, unique=True,
+                               nullable=False),
+                        Column(u'created', types.DateTime, default=datetime.datetime.now)
+                        )
+
 
 def setup():
     '''Model setup; ensure our table exists'''
-    if not ldap_user_table.exists() and user.user_table.exists():
+    if not ldap_user_table.exists() and model.user.user_table.exists():
         ldap_user_table.create()
 
-class LdapUser(domain_object.DomainObject):
+
+class LdapUser(model.domain_object.DomainObject):
     '''Represents an entry mapping a ldap id to a CKAN user'''
+
     @classmethod
     def by_ldap_id(cls, ldap_id, autoflush=True):
         '''Return the LdapUser object mapping the given ldap id
@@ -38,8 +41,8 @@ class LdapUser(domain_object.DomainObject):
         :returns: LdapUser object or None
 
         '''
-        obj = meta.Session.query(cls).autoflush(autoflush)\
-              .filter_by(ldap_id=ldap_id).first()
+        obj = model.meta.Session.query(cls).autoflush(autoflush) \
+            .filter_by(ldap_id=ldap_id).first()
         return obj
 
     @classmethod
@@ -51,15 +54,13 @@ class LdapUser(domain_object.DomainObject):
         :returns: LdapUser object or None
 
         '''
-        obj = meta.Session.query(cls).autoflush(autoflush)\
-              .filter_by(user_id=user_id).first()
+        obj = model.meta.Session.query(cls).autoflush(autoflush) \
+            .filter_by(user_id=user_id).first()
         return obj
 
-meta.mapper(LdapUser, ldap_user_table,
-       properties={
-            u'user': orm.relation(user.User,
-                backref=orm.backref(u'ldap_user',
-                cascade=u'all, delete, delete-orphan'
-                ))
-            },
-       )
+
+model.meta.mapper(LdapUser, ldap_user_table, properties={
+    u'user': orm.relation(model.user.User,
+                          backref=orm.backref(u'ldap_user',
+                                              cascade=u'all, delete, delete-orphan'))
+    }, )
