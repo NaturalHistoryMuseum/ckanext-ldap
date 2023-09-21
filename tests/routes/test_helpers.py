b68e13d7a209112182026171eec396de82544021
Josh Humphries <j.humphries@nhm.ac.uk>
2023-09-21 21:13:57 +0100

    test: add a test for login_success
    This is an important test as it is something that works on CKAN 2.9 but not on CKAN 2.10.

--- tests/routes/test_helpers.py
+++ tests/routes/test_helpers.py
@@ -3,7 +3,7 @@ from unittest.mock import patch, MagicMock
 import pytest
 
 from ckan.lib.helpers import url_for
-from ckanext.ldap.routes._helpers import login_failed
+from ckanext.ldap.routes._helpers import login_failed, login_success
 
 
 @pytest.mark.filterwarnings("ignore::sqlalchemy.exc.SADeprecationWarning")
@@ -19,3 +19,20 @@ def test_login_failed(flash_notice_mock: MagicMock, flash_error_mock: MagicMock)
     flash_error_mock.assert_called_once_with(error)
     assert response.status_code == 302
     assert response.location.endswith(url_for("user.login"))
+
+
+@pytest.mark.filterwarnings("ignore::sqlalchemy.exc.SADeprecationWarning")
+@pytest.mark.usefixtures("with_request_context")
+def test_login_success():
+    username = "a_user"
+    came_from = "somewhere_else"
+
+    with patch("ckanext.ldap.routes._helpers.session") as mock_session:
+        response = login_success(username, came_from)
+
+    mock_session.__setitem__.assert_called_once_with("ckanext-ldap-user", username)
+    assert mock_session.save.called
+    assert response.status_code == 302
+    assert response.location.endswith(
+        f'{url_for("user.logged_in")}?came_from={came_from}'
+    )
